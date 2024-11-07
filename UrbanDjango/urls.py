3c45d1c583f1b1d101bc1d13f5dc8f2e46b60d8b
VadimBichikhanov <bishikhan82@gmail.com>
2024-11-07 20:24:31 +0800

    first commit

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('task3.urls')),
+]
+
+'''
 from task2.views import class_based, function_based
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('class_based/', class_based),
     path('function_based/', function_based),
 ]
+'''
